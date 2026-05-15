afced994b284c812ad0a27de8a76f384b4123d74
imdohyeon <202230120@email.daelim.ac.kr>
2026-05-15 15:20:46 +0900

    Test

--- src/assets/components/Signup1.jsx
+++
@@ -1,8 +0,0 @@
-export default function Signup1() {
-    return (
-        <form onSubmit={() => alert("Submitting!")}>
-            <input />
-            <button>Send1</button>
-        </form>
-    )
-}
