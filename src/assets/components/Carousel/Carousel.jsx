a5813b4011899c6ac3f97671c61f18bc64b20762
imdohyeon <202230120@email.daelim.ac.kr>
2026-05-24 09:40:04 +0900

    0520

---
+++ src/assets/components/Carousel/Carousel.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { galleryImages } from "./imgData";
+import styles from "./Carousel.module.css"; 
+
+export default function Carousel() {
+    const [index, setIndex] = useState(0);
+
+    function handleNext() {
+        if (index === galleryImages.length - 1) {
+            setIndex(0);
+        } else {
+            setIndex(index + 1);
+        }
+    }
+
+    function handlePrevious() {
+        if (index === 0) {
+            setIndex(galleryImages.length - 1); 
+        } else {
+            setIndex(index - 1);
+        }
+    }
+
+    let slide = galleryImages[index];
+    
+    return (
+        <div className={styles.carouselContainer}> 
+            
+            <div className={styles.buttonGroup}>
+                <button className={styles.button} onClick={handlePrevious}>Previous</button>
+                <button className={styles.button} onClick={handleNext}>Next</button>
+            </div>
+            
+            <h2 className={styles.title}>
+                <i>{slide.name}</i> by {slide.artist}
+            </h2>
+
+            <h3 className={styles.counter}>
+                ({index + 1} of {galleryImages.length})
+            </h3>        
+
+            <img className={styles.image} src={slide.url} alt={slide.alt} />
+            <p className={styles.description}>{slide.description}</p>
+        </div>
+    );
+}
---
+++ src/assets/components/Carousel/index.jsx
@@ -0,0 +1,4 @@
+import slider1 from "./img/1.jpg"
+import slider2 from "./img/2.jpg"
+
+export const slide = {slider1, slider2};
